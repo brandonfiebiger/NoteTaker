05b60d00d6ffe4282e6b5de160f93302cf6f2a2b
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-12-26 13:55:59 -0700

    Fix naming in NoteCard

--- front-end/src/Components/NoteCard/NoteCard.js
+++ front-end/src/Components/NoteCard/NoteCard.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const NoteCard = props => (
 
   <li>
-    <p>{props.Date}</p>
-    <p>{props.Tag}</p>
-    <p>{props.Content}</p>
+    <p>{ props.date }</p>
+    <p>{ props.tag }</p>
+    <p>{ props.content }</p>
   </li>
 )
