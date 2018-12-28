feb9dfb1a9f971030b2d45275ac28ef10b800349
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-12-27 20:38:29 -0700

    Add createNote function to serverless

---
+++ serverless-rest-api/notetaker/create.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+
+  const data = JSON.parse(event.body);
+
+  if (typeof data.content !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldnt create the note!'));
+    return;
+  }
+
+  const params = {
+    TableName: 'notetaker',
+    Item: {
+      id: uuid.v1(),
+      content: data.content,
+      date: data.date,
+      tag: data.tag
+    }
+  }
+
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      callback(new Error('Couldnt create note'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+    callback(null, response);
+  })
+}
